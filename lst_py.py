5abf181d8d46f190ebcec5cd0af536eced834ce8
David Itehua Xalamihua <davidix1991@gmail.com>
2024-09-06 17:39:26 -0600

    repositorio finalizado

---
+++ lst_py.py
@@ -0,0 +1,12 @@
+
+lst_urls= [
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FET068904CO-107348', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FER101609CO-105318', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FER100464CO-105262', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FER044634CO-104778', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FET102852CO-521468', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FET100922CO-520148', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FER034476CO-104437', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FER040770CO-520374', 
+'https://rpc.ift.org.mx/vrpc/RpcSearchController/showConcesionInfo?idConcesion=FER102758CO-105262', 
+]
